49e796578a93e00116e5c4bd13bf4e3f00e48c72
cedaret <42737298+cedaret@users.noreply.github.com>
2018-09-02 22:36:07 +0100

    Add files via upload

---
+++ A3_10661102.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	int mark;
+
+ 
+
+cout<<"Enter your mark: ";
+
+cin>>mark;
+
+if (mark < 40)
+
+cout<<"Your grade is F";
+
+else if (mark < 50)
+
+cout<<"Your grade is E";
+
+else if (mark < 60)
+
+cout<<"Your grade is D";
+
+else if (mark < 70)
+
+cout<<"Your grade is C";
+
+else if (mark < 80)
+
+cout<<"Your grade is B";
+
+else
+
+cout<<"Your grade is A";
+
+cout<<"\n";
+	
+	
+	
+	return 0;
+}
